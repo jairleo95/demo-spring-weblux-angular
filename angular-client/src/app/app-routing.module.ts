1a362b9adf2d33d4d743e0489a304b19729f27a9
Jair Santos <jairleo95@gmail.com>
2022-04-29 20:30:07 -0500

    release

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
+import { SearchCustomersComponent } from './customers/search-customers/search-customers.component';
+
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'customers', pathMatch: 'full' },
+  { path: 'customers', component: CustomersListComponent },
+  { path: 'add', component: CreateCustomerComponent },
+  { path: 'search', component: SearchCustomersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { CustomerService } from './customers/customer.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
+import { CustomersDetailsComponent } from './customers/customers-details/customers-details.component';
+import { SearchCustomersComponent } from './customers/search-customers/search-customers.component';
+import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersListComponent,
+    CustomersDetailsComponent,
+    SearchCustomersComponent,
+    CreateCustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
